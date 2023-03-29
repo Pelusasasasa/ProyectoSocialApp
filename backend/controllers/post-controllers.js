6b4084fdcefcc132df8a53f2abf874bfac6c7176
Pelusa-not <agustinlorenzatto@gmail.com>
2023-03-29 09:17:50 -0300

    Hice el controllers de post

---
+++ backend/controllers/post-controllers.js
@@ -0,0 +1,32 @@
+const postCTRL = {};
+
+const Post = require('../models/Post');
+
+postCTRL.post = async(req,res)=>{
+    const post = new Post(req.body);
+    await post.save();
+    console.log(`Post del usuario ${req.body.user} cargado`);
+    res.end();
+}
+
+postCTRL.getForId = async(req,res)=>{
+    const {id} = req.params;
+    const post = await Post.find({_id:id});
+    res.send(post)
+};
+
+postCTRL.putForId = async(req,res)=>{
+    const {id} = req.params;
+    await Post.findOneAndUpdate({_id:id},req.params);
+    console.log(`Post de ${req.body.user} Modificado `);
+    res.end();
+}
+
+postCTRL.deleteForId = async(req,res)=>{
+    const {id} = req.params;
+    await Post.findOneAndDelete({_id:id});
+    console.log(`Post con id ${id} Eliminado`)
+    res.end();
+}
+
+module.exports = postCTRL
